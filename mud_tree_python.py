fd098360063cadf654e3d8e140d15d49761f9a33
Guillaume Yziquel <guillaume@vizyon.ai>
2023-08-05 23:47:07 +0200

    Validating dataset name to avoid string injection.

--- mud_tree_python.py
+++ mud_tree_python.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
+import sys
 import subprocess
 
 def main():
-    zfs_list = subprocess.run("zfs list -H -o name", shell=True, stdout=subprocess.PIPE, text=True).stdout.splitlines()
-    print(zfs_list) 
+    try:
+        datasets = subprocess.run("zfs list -H -o name", shell=True, stdout=subprocess.PIPE, text=True).stdout.splitlines()
+        for dataset in datasets:
+            validate_dataset_name(dataset)
+        print(datasets)
+    except InvalidDatasetName as e:
+        print("Exception:", str(e), file=sys.stderr)
+
+import re
+
+def validate_dataset_name(dataset_name):
+    if re.match(r'^[a-zA-Z0-9/_]+$', dataset_name) is None:
+        raise InvalidDatasetName(dataset_name)
+
+class InvalidDatasetName(Exception):
+   pass
